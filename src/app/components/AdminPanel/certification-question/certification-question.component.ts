9377f77562edd16c37248c23ed15f549480f8835
ShaimaaAKamal <shaimaakamal039@gmail.com>
2026-01-31 02:12:22 +0200

    initial component

--- src/app/components/AdminPanel/certification-question/certification-question.component.ts
+++ src/app/components/AdminPanel/certification-question/certification-question.component.ts
@@ -1,11 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { SpkNgSelectComponent } from '../../../shared/spk-ng-select/spk-ng-select.component';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { InputComponent } from '../../../shared/input/input.component';
+import { FileUploadComponent } from '../../../shared/file-upload/file-upload.component';
+import { CheckboxComponent, CheckboxOption } from '../../../shared/checkbox/checkbox.component';
 
 @Component({
   selector: 'app-certification-question',
-  imports: [SpkNgSelectComponent, ButtonComponent],
+  imports: [SpkNgSelectComponent, ButtonComponent, InputComponent,
+    FileUploadComponent, ReactiveFormsModule, CheckboxComponent],
   templateUrl: './certification-question.component.html',
   styleUrl: './certification-question.component.scss'
 })
@@ -32,13 +36,46 @@ export class CertificationQuestionComponent {
     { label: "8", value: 8 },
     { label: "9", value: 9 },
   ];
+
+  questionMarks = [
+    { label: "1", value: 1 },
+    { label: "2", value: 2 },
+    { label: "3", value: 3 },
+    { label: "4", value: 4 },
+    { label: "5", value: 5 },
+    { label: "6", value: 6 },
+    { label: "7", value: 7 },
+    { label: "8", value: 8 },
+    { label: "9", value: 9 },
+  ];
+
+  examModes=[
+    {label:"Practice Mode",value:"practice"},
+    {label :"Exam Mode",value:"exam"}
+  ]
+  checkboxOptions: CheckboxOption []=[
+    {label:"" , value:"true"}
+  ]
   fb = inject(FormBuilder);
   form = this.fb.group({
     certification: ['', Validators.required],
+    examNo: ['', Validators.required],
+    examMode: ['', Validators.required],
     questionType: [0, Validators.required],
+    questionHeader: ['', Validators.required],
+    questionOrder: ['', Validators.required],
+    questionMark: [0, Validators.required],
+    files: [[] as File[]],
+    answer: ['', Validators.required],
+    answers: ['', Validators.required],
+
+
   });
 
   onAddNewCertification(){
 
   }
+  onActivateAddAnswerSection(){}
+  cancel(){}
+  onSubmit(){}
 }
--- src/app/shared/checkbox/checkbox.component.ts
+++ src/app/shared/checkbox/checkbox.component.ts
@@ -1,79 +1,9 @@
-// import { Component, input, signal, computed } from '@angular/core';
-// import {
-//   ControlValueAccessor,
-//   NG_VALUE_ACCESSOR,
-// } from '@angular/forms';
-
-// @Component({
-//   selector: 'app-checkbox',
-//   imports: [],
-//   templateUrl: './checkbox.component.html',
-//   styleUrl: './checkbox.component.scss',
-//   providers: [
-//     {
-//       provide: NG_VALUE_ACCESSOR,
-//       useExisting: CheckboxComponent,
-//       multi: true,
-//     },
-//   ],
-// })
-// export class CheckboxComponent implements ControlValueAccessor {
-//   // Inputs
-//   label = input<string>('Checkbox');
-//   id = input<string>('');   // optional – auto-generated if empty
-//   disabled = input<boolean>(false);
-
-//   // Internal signals
-//   checked = signal<boolean>(false);
-//   isTouched = signal<boolean>(false);
-
-//   // Auto-generate unique ID if not provided
-//   effectiveId = computed(() => {
-//     const userId = this.id();
-//     return userId || `checkbox-${Math.random().toString(36).substring(2, 11)}`;
-//   });
-
-//   // CVA callbacks
-//   onChange: (value: boolean) => void = () => { };
-//   onTouched: () => void = () => { };
-
-//   writeValue(value: boolean | null | undefined): void {
-//     this.checked.set(!!value);
-//   }
-
-//   registerOnChange(fn: (value: boolean) => void): void {
-//     this.onChange = fn;
-//   }
-
-//   registerOnTouched(fn: () => void): void {
-//     this.onTouched = fn;
-//   }
-
-//   setDisabledState(isDisabled: boolean): void {
-//     // Input already handles it, but can sync if needed
-//   }
-
-//   toggle(): void {
-//     if (this.disabled()) return;
-
-//     this.checked.update(v => !v);
-//     this.onChange(this.checked());
-//     this.isTouched.set(true);
-//     this.onTouched();
-//   }
-
-//   onBlur(): void {
-//     this.isTouched.set(true);
-//     this.onTouched();
-//   }
-// }
-
 import { Component, computed, input, signal } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 export interface CheckboxOption {
   label: string;
-  value: string;   // or number if your IDs are numeric
+  value: string;   
 }
 
 @Component({
